4583194537bdf4b3fb91b237a3d8441024087f4c
mnovok <mnovok00@fesb.hr>
2023-10-18 09:47:22 +0200

    Envirornment setup

---
+++ semester-project/src/app/showcase/page.tsx
@@ -0,0 +1,10 @@
+function ShowcasePage() {
+    return(
+        <div>
+            <h1>Showcase</h1>
+            <p>This is showcase page</p>
+        </div>
+    );
+}
+
+export default ShowcasePage;
