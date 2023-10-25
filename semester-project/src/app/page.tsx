64f475292562744d575cd24126619ce93fc10cef
ninarada <nradja00@fesb.hr>
2023-10-25 11:20:04 +0200

    .

--- semester-project/src/app/page.tsx
+++ semester-project/src/app/page.tsx
@@ -3,11 +3,20 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="">
-      <div className={styles.landing}>
+    <div className={styles.main}>
+      <div className={styles.landingFrame1}>
         <p className={styles.slogan}>Navigating Your Journey, <br /> One Adventure at a Time!</p>
         <Link href="/destinations" className={styles.btn}>Explore Now</Link>
       </div>
-    </main>
+      <div className={styles.discoverFrame2}>
+        <div className={styles.discover}>
+          <h1 className={styles.discoverTitle}>Discover</h1>
+          <p className={styles.discoverText}>Unveil the allure of enchanting cities. Immerse yourself in diverse cultures, savor local flavors, and create unforgettable memories.</p>
+        </div>
+        <div className={styles.destinationsSlider}>
+          
+        </div>
+      </div>
+    </div>
   );
 }
