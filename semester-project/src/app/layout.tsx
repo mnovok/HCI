8d3d5d2855889ebee0c9f2b847a3e259df6877ea
ninarada <nradja00@fesb.hr>
2023-10-25 12:09:33 +0200

    .

--- semester-project/src/app/layout.tsx
+++ semester-project/src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         {children}
 
         
-      <footer className="bg-white dark:bg-gray-900">
+      <footer className="bg-[#d9e3de] dark:bg-gray-900">
           <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
               <div className="md:flex md:justify-between">
                 <div className="mb-6 md:mb-0">
@@ -103,7 +103,7 @@ export default function RootLayout({
                     </div>
                 </div>
             </div>
-            <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <hr className="my-6 border-gray-500 sm:mx-auto dark:border-gray-700 lg:my-8" />
             <div className="sm:flex sm:items-center sm:justify-between">
             <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400"> Copyright © 2023. All rights reserved</span>
             </div>
